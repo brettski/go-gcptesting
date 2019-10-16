2f66962ed42fddfe8434e0b28986d1c5394d3d7c
Brett Slaski <brettski@yahoo.com>
2019-10-16 16:00:55 -0500

    Add post support

--- nodesvr/index.js
+++ nodesvr/index.js
@@ -7,6 +7,9 @@ exports.getUSome = (req, res) => {
     if (req.method=='GET') {
         res.send(`That's a get with ${escape(req.query.q)} <= q`)
     }
+    else if (req.method=='POST') {
+        res.send(`That's a post with q:${escape(req.query.q)} and body:${req.body}`)
+    }
     else {
         res.status(400)
         res.send(`not sure what to do with that`)

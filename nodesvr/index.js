a912f0644a08168e66445bc15b7449efd9128663
Brett Slaski <brettski@yahoo.com>
2019-10-17 12:57:43 -0500

    some dumb change

--- nodesvr/index.js
+++ nodesvr/index.js
@@ -5,7 +5,7 @@ const escape = require('escape-html')
 exports.getUSome = (req, res) => {
     console.log(req.method)
     if (req.method=='GET') {
-        res.send(`That's a get with ${escape(req.query.q)} <= q`)
+        res.send(`You GET what you get with me. :: ${escape(req.query.q)} <= q`)
     }
     else if (req.method=='POST') {
         res.send(`That's a post with q:${escape(req.query.q)} and body:${req.body}`)
